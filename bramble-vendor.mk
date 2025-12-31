b14dadfa7f896aa3548fb55459538313d4895461
Michael Bestas <mkbestas@lineageos.org>
2025-12-31 03:04:21 +0200

    bramble: Use libprotobuf-cpp-{lite,full}-21.7 from hardware/lineage/compat
    Change-Id: I07eb8e11057f254bfdaaf3af128d526c9d127ae1

--- bramble-vendor.mk
+++ bramble-vendor.mk
@@ -1220,8 +1220,6 @@ PRODUCT_PACKAGES += \
     libperipheral_client \
     libpowercallback \
     libpowercore \
-    libprotobuf-cpp-full-21.7 \
-    libprotobuf-cpp-lite-21.7 \
     libpvr \
     libqcbor \
     libqcc_file_agent \
